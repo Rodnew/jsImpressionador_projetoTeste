0a7cab4663c3a151bf74b61dd59739a26d285bde
Rodrigo Moura <rodrigo.joplin@gmail.com>
2024-09-24 03:14:07 -0300

    atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o Github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
